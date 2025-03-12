330c149a9567f6872c8daef4f7d07f1e60a89008
Ryan Mikulec <ryan.mikulec@mikulec.net>
2025-03-12 01:18:04 -0400

    Switched to nextjs

---
+++ src/next.config.ts
@@ -0,0 +1,33 @@
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "rmikulec.github.io";
+
+const nextConfig: NextConfig = {
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  //basePath: isProd ? `/${repoName}` : undefined,
+  //assetPrefix: isProd ? `/${repoName}/` : undefined,
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
